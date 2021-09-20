6a1e4e788def1efbaff5a027c6205e19563b08ca
Bob <>
2021-09-20 14:08:49 -0500

    minor access control tweak

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ async function startServer() {
 
   // Add Access-Control-Allow-Origin Headers
   app.use((req,res,next) => {
-    res.setHeader("Access-Control-Allow-Origin",["https://frontend-cleaning.vercel.app", "http://localhost:1111", "https://cosvid-frontend.vercel.app"]);
+    res.setHeader("Access-Control-Allow-Origin",["https://frontend-cleaning.vercel.app" || "http://localhost:1111" || "https://cosvid-frontend.vercel.app"]);
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
